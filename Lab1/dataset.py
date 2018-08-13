ff7e708c6cd23ddc28c6df10354c019a45bf932b
Bapi Gupta <rajat310198@outlook.com>
2018-08-14 01:58:59 +0530

    some initial code

---
+++ Lab1/dataset.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import os, re
+
+# Cat 0
+# Dog 1
+
+class CatDog(object):
+
+    def __init__(self, dir_name='/dataset'):
+        self.data_dir = dir_name
+        self.train_images, self.train_labels  = self.read_train_images()
+        #self.test_images, self.test_labels  = self.read_test_images()
+        print(self.train_images.shape, self.train_labels.shape)
+
+    def read_train_images(self):
+        train_images = os.listdir(os.getcwd() + self.data_dir +'/train')
+        train_images_list = []
+        train_labels_list = []
+        for image in train_images:
+            img = cv2.imread(image)
+            train_images_list.append(img)
+            if image == re.match('cat', image):
+                train_labels_list.append(0)
+            else:
+                train_labels_list.append(1)
+        return np.array(train_images_list), self.one_hot(train_labels_list)
+        
+
+    def one_hot(self, labels):
+        labels = np.array(labels)
+        b = np.zeros((len(labels), 3))
+        b[np.arange(len(labels)), labels] = 1
+        return b
+
+
+    def read_test_images(self):
+        pass
+
+
+
+
